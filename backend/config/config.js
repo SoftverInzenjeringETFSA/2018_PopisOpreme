76038eec4d0d5a6a47b93ce8b8d46fca2eb5b262
Ald0 <aselimovic@outlook.com>
2018-05-17 01:04:19 +0200

    Adds mlab database connection string

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,6 +2,7 @@ const
         mongoose    = require('mongoose'), 
         Schema      = mongoose.Schema; 
 
-mongoose.connect("mongodb://localhost/PopisOpreme");
+//mongoose.connect("mongodb://localhost/PopisOpreme");
+mongoose.connect("mongodb://admin:password@ds125680.mlab.com:25680/popisopreme");
 
 module.exports = { Schema, mongoose };
