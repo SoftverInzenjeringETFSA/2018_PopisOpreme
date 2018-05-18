1f9077d430c21fa98a0d5eb43c20814252e9be20
Haris Palic <haris.palic@gmail.com>
2018-05-18 03:07:14 +0200

    dodavanje prostorije, ispravljene greske

--- backend/controllers/prostorije/prostorije.js
+++ backend/controllers/prostorije/prostorije.js
@@ -9,13 +9,9 @@ router.use(bodyParser.urlencoded({ extended: true }));
 // router.use(cors());
 
 // WebStorm ima problem sa prepoznavanjem modula, pa sam ovo uradio da zaobidjem problem. (var Room;)
-<<<<<<< Updated upstream
-//var Room;
- const Room = require('../../models/Room');
-=======
+
 // var Room;
-const Room = require('../models/User');
->>>>>>> Stashed changes
+const Room = require('../../models/Room');
 
 router.post('/addRoom', function (req, res)
 {
@@ -45,8 +41,6 @@ router.post('/deleteRoom', function (req, res)
     res.send('OK remove');
 });
 
-<<<<<<< Updated upstream
-=======
+
 // Room = require('../models/Room');
->>>>>>> Stashed changes
 module.exports = router;
