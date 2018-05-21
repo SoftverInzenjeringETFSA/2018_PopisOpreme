54cb4f86f345578954cd3ba89411fb310a13bb7b
Haris Palic <haris.palic@gmail.com>
2018-05-21 19:59:19 +0200

    Zavrsena druga komponenta

--- backend/controllers/prostorije/prostorije.js
+++ backend/controllers/prostorije/prostorije.js
@@ -1,16 +1,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
-// var cors = require('cors');
-
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
-// router.use(cors());
+router.use(bodyParser.urlencoded({extended: true}));
 
-// WebStorm ima problem sa prepoznavanjem modula, pa sam ovo uradio da zaobidjem problem. (var Room;)
-
-// var Room;
 const Room = require('../../models/Room');
 
 router.post('/addRoom', function (req, res)
@@ -18,29 +12,143 @@ router.post('/addRoom', function (req, res)
     var roomName = req.body.roomName;
     var parentRoomName = req.body.parentRoomName;
 
-    Room.create({
-        roomName: roomName,
-        parentRoomName: parentRoomName,
-    }, function (err, data)
+    Room.findOne({
+        roomName: roomName
+    }, function (err, room)
     {
         if (err)
         {
             console.log(err);
+            res.send(err);
         }
         else
         {
-            console.log(data);
-            res.send(data);
+            if (room)
+            {
+                console.log('Postoji prostorija sa tim imenom!');
+                res.send('Postoji prostorija sa tim imenom!');
+            }
+            else
+            {
+                Room.findOne({
+                    roomName: parentRoomName
+                }, function (err, room)
+                {
+                    if (err)
+                    {
+                        console.log(err);
+                        res.send(err);
+                    }
+                    else
+                    {
+                        if (room)
+                        {
+                            Room.create({
+                                roomName: roomName,
+                                parentRoomName: parentRoomName,
+                            }, function (err, data)
+                            {
+                                if (err)
+                                {
+                                    console.log(err);
+                                }
+                                else
+                                {
+                                    console.log('Uspjesno dodana prostorija');
+                                    res.send(data);
+                                }
+                            });
+                        }
+                        else
+                        {
+                            console.log('Ne postoji parent prostorija');
+                            res.send('Ne postoji parent prostorija');
+                        }
+                    }
+                });
+            }
         }
     });
 });
 
-router.post('/deleteRoom', function (req, res)
+router.get('/getRooms', function (req, res)
 {
-    // Uklanjanje iz baze
-    res.send('OK remove');
+    Room.find(function (err, rooms)
+    {
+        if (err)
+        {
+            console.log(err);
+            res.send(err);
+        }
+        else
+        {
+            // res.setHeader('Content-Type', 'application/json');
+            res.send(rooms);
+        }
+    })
 });
 
+router.post('/getRoom', function (req, res)
+{
+    Room.findOne({
+        roomName: req.body.roomName,
+    }, function (err, room)
+    {
+        if (err)
+        {
+            console.log(err);
+            res.send(err);
+        }
+        else
+        {
+            if (room)
+                res.status(200);
+            else res.status(204);
+            res.send(room);
+        }
+    })
+});
 
-// Room = require('../models/Room');
+router.delete('/deleteRoom', function (req, res)
+{
+    const roomName = req.body.roomName;
+    console.log(roomName);
+    var parent = null;
+    Room.findOne({
+        roomName: roomName
+    }, function (err, room)
+    {
+        if (err)
+        {
+            console.log(err);
+            res.send(err);
+        }
+        else
+        {
+            console.log(room);
+            if (!room)
+            {
+                console.log('Ne postoji ta prostorija');
+                res.send('Ne postoji prostorija');
+            }
+            else
+            {
+                parent = room.parentRoomName;
+                Room.deleteOne({
+                    roomName: roomName
+                }, function (err)
+                {
+                    if (err)
+                    {
+                        console.log(err);
+                        res.send(err);
+                    }
+                    console.log('Uspjesno obrisana prostorija');
+                    res.send('Uspjesno obrisana prostorija ' +  roomName + '. Parent prostorija je ' + parent + '.');
+                });
+            }
+        }
+    });
+});
+
 module.exports = router;
--- frontend/src/components/prostorija/index.js
+++ frontend/src/components/prostorija/index.js
@@ -5,22 +5,193 @@ class BrisanjeProstorije extends React.Component
     constructor(props)
     {
         super(props);
+
+        this.state = {
+            name: '',
+            roomList: []
+        };
+
+        this.handleDeleteRoom = this.handleDeleteRoom.bind(this);
+        this.onChangeName = this.onChangeName.bind(this);
+        this.getRooms = this.getRooms.bind(this);
+        this.handleListAllRooms = this.handleListAllRooms.bind(this);
+        this.handleSearchList = this.handleSearchList.bind(this);
+    }
+
+    componentDidMount()
+    {
+        // this.getRooms();
+    }
+
+    getRooms()
+    {
+        fetch('http://localhost:8080/getRooms', {
+            headers: {
+                'Accept': 'application/json'
+            },
+        }).then(function (res)
+        {
+            return res.json();
+        })
+            .then((res) =>
+            {
+                this.setState({
+                    roomList: res,
+                })
+            });
+
+    }
+
+    onChangeName(e)
+    {
+        this.setState({
+            name: e.target.value,
+        });
+    }
+
+    handleDeleteRoom(e, room)
+    {
+        e.preventDefault();
+
+        const newRoomList = this.state.roomList.filter(r =>
+        {
+            return r !== room;
+        });
+
+        this.setState({
+            roomList: newRoomList,
+        });
+
+        var myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+
+        const options = {
+            method: 'DELETE',
+            headers: myHeaders,
+            body: JSON.stringify(room),
+        };
+
+        var myRequest = new Request('http://localhost:8080/deleteRoom', options);
+        const response = fetch(myRequest);
+        alert('Uspjesno obrisana prostorija');
+
+    }
+
+    handleListAllRooms(e)
+    {
+        e.preventDefault();
+
+        this.getRooms();
+    }
+
+    handleSearchList(e)
+    {
+        e.preventDefault();
+        fetch('http://localhost:8080/getRoom', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }, body: JSON.stringify({
+                roomName: this.state.name,
+            })
+        }).then(function (res)
+        {
+            if (res.status !== 204)
+            {
+                return res.json();
+            }
+            else
+            {
+                return null;
+            }
+        })
+            .then((res) =>
+            {
+                if (res === null)
+                {
+                    alert('Nema prostorije sa tim imenom');
+                }
+                else
+                {
+                    var emptyArray = [];
+                    emptyArray.push(res);
+                    this.setState({
+                        roomList: emptyArray,
+                    })
+                }
+            });
     }
 
     render()
     {
+        const roomList = this.state.roomList;
+        const buttonBox = {
+            textAlign: 'center',
+        };
         return (
             <div className={'container'}>
                 <div className="row">
-                    <div className="col-lg-offset-4 col-lg-4">
+                    <div className="col-lg-12">
                         <form className={'well well-lg'}>
                             <div className="form-group">
                                 <label>Pretraga</label>
-                                <input className="form-control" name="roomName" placeholder={'Pretrazi'}/>
+                                <input className="form-control" name="roomName" placeholder={'Pretrazi'}
+                                       onChange={this.onChangeName}/>
                             </div>
-                            <button className="btn btn-default" type="submit" onClick={this.onAddRoom}>
-                                Dodaj prostoriju
-                            </button>
+                            <div className="button-box col-lg-12" style={buttonBox}>
+                                <button
+                                    className="btn btn-default"
+                                    type="submit"
+                                    onClick={this.handleSearchList}
+                                    style={{marginBottom: 10, float: 'left'}}>
+                                    Pretrazi
+                                </button>
+                                <button
+                                    className="btn btn-default"
+                                    type="submit"
+                                    onClick={this.handleListAllRooms}
+                                    style={{marginBottom: 10, float: 'right'}}>
+                                    Izlistaj sve prostorije
+                                </button>
+                            </div>
+
+                            <header>
+                                <h2 className="text-center">Lista prostorija</h2>
+                            </header>
+                            <table className={'table'}>
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Naziv prostorije</th>
+                                    <th>Naziv parent prostorije</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    roomList.map((room, i) =>
+                                    {
+                                        return (
+                                            <tr key={room._id}>
+                                                <th>{i + 1}</th>
+                                                <td>{room.roomName}</td>
+                                                <td>{room.parentRoomName}</td>
+                                                <td>
+                                                    <button
+                                                        className={'btn btn-danger'}
+                                                        onClick={(e) => this.handleDeleteRoom(e, room)}
+                                                        type={'button'}
+                                                        style={{float: 'right'}}
+                                                    >
+                                                        Obriši
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                                </tbody>
+                            </table>
                         </form>
                     </div>
                 </div>
@@ -37,25 +208,50 @@ class UnosProstorije extends React.Component
         this.state = {
             roomName: '',
             parentRoomName: '',
+            errorMsg: '',
+            successMsg: '',
         };
         this.onChangeRoomName = this.onChangeRoomName.bind(this);
         this.onChangeParentRoomName = this.onChangeParentRoomName.bind(this);
         this.onAddRoom = this.onAddRoom.bind(this);
+        this.validateName = this.validateName.bind(this);
+        this.setError = this.setError.bind(this);
+        this.unsetError = this.unsetError.bind(this);
     }
 
-    onAddRoom()
+    validateName(name)
     {
+        var regex = new RegExp('^[a-zA-Z0-9|/|(|)|-]+$');
+        return regex.test(name);
+    }
+
+    unsetError()
+    {
+        this.setState({
+            errorMsg: ''
+        });
+    }
+
+    setError()
+    {
+        this.setState({
+            errorMsg: 'Naziv prostorije može sadržavati samo alfanumeričke znakove i /()-'
+        });
+    }
+
+    onAddRoom(e)
+    {
+        e.preventDefault();
 
         var data = {
             roomName: this.state.roomName,
             parentRoomName: this.state.parentRoomName
         };
 
-        var regex = new RegExp('^[a-zA-Z0-9|/|(|)|-]+$');
 
-        if (!regex.test(data.roomName))
+        if (!this.validateName(data.roomName))
         {
-            alert('Naziv prostorije nije validan!');
+            alert('Nije validan naziv prostorije!')
         }
         else
         {
@@ -81,6 +277,10 @@ class UnosProstorije extends React.Component
         this.setState({
             roomName: e.target.value
         });
+        if (!this.validateName(e.target.value) && e.target.value !== '')
+            this.setError();
+        else
+            this.unsetError();
     }
 
     onChangeParentRoomName(e)
@@ -111,12 +311,27 @@ class UnosProstorije extends React.Component
                                        placeholder={'Naziv parent prostorije'}
                                        onChange={this.onChangeParentRoomName}/>
                             </div>
-                            <button className="btn btn-default" type="submit" onClick={this.onAddRoom}>
-                                Dodaj prostoriju
-                            </button>
+                            <div className={'form-group'}>
+                                <button className="btn btn-default" type="submit" onClick={this.onAddRoom}>
+                                    Dodaj prostoriju
+                                </button>
+                            </div>
+                            {
+                                this.state.successMsg &&
+                                <div className="alert alert-success" style={{paddingTop: 10}}>
+                                    <strong>{this.state.successMsg}</strong>
+                                </div>
+                            }
+                            {
+                                this.state.errorMsg &&
+                                <div className="alert alert-danger">
+                                    <strong>{this.state.errorMsg}</strong>
+                                </div>
+                            }
                         </form>
                     </div>
                 </div>
+
             </div>
         )
     }
