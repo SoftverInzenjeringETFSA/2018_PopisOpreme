7aabac81eb8bc72c575d2f7f2c15be34918305f6
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-05-23 18:00:12 +0200

    fixed

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -6,96 +6,88 @@ import Login from '../login';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-default navbar-inverse">
-                <ul className="nav navbar-nav">
-                    <li>
-                        <Link to="/login">
-                            Login Page
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/register">
-                            Register Page
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Home Page
-                        </Link>
-                    </li>
+            <ul className="nav navbar-nav">
+                <li>
+                    <Link to="/login">
+                        Login Page
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/register">
+                        Register Page
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        Home Page
+                    </Link>
+                </li>
 
-                    <li className="dropdown">
-                        <a className="dropdown-toggle" data-toggle="dropdown" href="#">Categories
-                            <span className="caret"></span></a>
-                        <ul className="dropdown-menu">
-                            <li>
-                                <Link to="/categories">
-                                    List All
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/categories/add">
-                                    Add new
-                                </Link>
-                            </li>
-                        </ul>
-                    </li>
+                <li className="dropdown">
+                    <a className="dropdown-toggle" data-toggle="dropdown" href="#">Categories
+                        <span className="caret"></span></a>
+                    <ul className="dropdown-menu">
+                        <li>
+                            <Link to="/categories">
+                                List All
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/categories/add">
+                                Add new
+                            </Link>
+                        </li>
+                    </ul>
+                </li>
 
-                    <li>
-                        <Link to="/unosProstorije">
-                            Unos prostorije
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/brisanjeProstorije">
-                            Brisanje prostorije
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/generisanjenaljepnica">
-                            Generisanje Naljepnica
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/otpisinventurnestavke">
-                            Otpis Inventurne Stavke
-                        </Link>
-                    </li>
-                    <li className="dropdown">
-                        <a className="dropdown-toggle" data-toggle="dropdown" href="#">Stavke
-                            <span className="caret"></span></a>
-                        <ul className="dropdown-menu">
-                            <li>
-                                <Link to="/dodajstavku">
-                                    Dodaj novu stavku
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/modifikujstavku">
-                                    Modifikuj stavku
-                                </Link>
-                            </li>
-                        </ul>
-                    <li>
-                        <Link to="/dodajstavku">
-                            Dodavanje Inventurne Stavke
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/modifikujstavku">
-                           Modifikacija Inventurne Stavke
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/novaInventura">
-                            Inventura
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/help">
-                            <img height="25px" src="https://unpkg.com/@icon/font-awesome/icons/question-circle.svg" alt="help"/>
-                        </Link>
-                    </li>
-                </ul>
+                <li>
+                    <Link to="/unosProstorije">
+                        Unos prostorije
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/brisanjeProstorije">
+                        Brisanje prostorije
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/generisanjenaljepnica">
+                        Generisanje Naljepnica
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/otpisinventurnestavke">
+                        Otpis Inventurne Stavke
+                    </Link>
+                </li>
+                <li className="dropdown">
+                    <a className="dropdown-toggle" data-toggle="dropdown" href="#">Stavke
+                        <span className="caret"></span></a>
+                    <ul className="dropdown-menu">
+                        <li>
+                            <Link to="/dodajstavku">
+                                Dodaj novu stavku
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/modifikujstavku">
+                                Modifikuj stavku
+                            </Link>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <Link to="/novaInventura">
+                        Inventura
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/help">
+                        <img height="25px" src="https://unpkg.com/@icon/font-awesome/icons/question-circle.svg"
+                             alt="help"/>
+                    </Link>
+                </li>
+            </ul>
         </nav>
     );
 };
